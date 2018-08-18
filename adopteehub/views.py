65867a26fc1f22361e1b1208ab32ddb79ca3ead4
David Moschkau <davidm@mhccom.com>
2018-08-17 20:53:23 -0500

    AdopeeHub first draft

---
+++ adopteehub/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'adopteehub/home.html')
+    
+def about(request):
+    return render(request,'adopteehub/about.html')
+    
+def board(request):
+    return render(request,'adopteehub/board.html')
+    
+def services(request):
+    return render(request,'adopteehub/services.html')
+    
+def getinvolved(request):
+    return render(request,'adopteehub/getinvolved.html')
