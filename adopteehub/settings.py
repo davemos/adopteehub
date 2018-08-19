7eb4f71941db2b27504f444ffb0ff6b8529ab366
David Moschkau <davidm@mhccom.com>
2018-08-19 13:53:58 -0500

    added static_root

--- adopteehub/settings.py
+++ adopteehub/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'adopteehub.urls'
@@ -118,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, "static")
 ]
